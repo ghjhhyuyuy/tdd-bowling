2e8cb44d3dc1e8ed333a9a140ba3c74ae1f8dd5e
ghjhh <798389920@qq.com>
2020-08-03 22:10:38 +0800

    [WangZuowen]style:Restyle method score.

--- src/main/java/twu/BowlingGame.java
+++ src/main/java/twu/BowlingGame.java
@@ -1,26 +1,31 @@
 package twu;
 
-import java.util.List; /**
+import java.util.List;
+
+/**
  * Created by wzw on 2020/8/3
  *
  * @author wzw
  */
 public class BowlingGame {
     public int score(List<Frame> frameList) throws Exception {
-        if(frameList == null || frameList.isEmpty()){
+        if (frameList == null || frameList.isEmpty()) {
             throw new Exception("input is wrong");
         }
         int score = 0;
-        for (int i = 0; i < frameList.size() - 1; i++) {
-            if(frameList.get(i).getThrowNumber() == 1){
-                score += sumStrike(frameList,i);
-            }else if(frameList.get(i).sumPinNumber() == 10){
-                score += sumSpare(frameList,i);
-            }else {
+        int strikeThrowNumber = 1;
+        int fullScore = 10;
+        int theLastFrameIndex = frameList.size() - 1;
+        for (int i = 0; i < theLastFrameIndex; i++) {
+            if (frameList.get(i).getThrowNumber() == strikeThrowNumber) {
+                score += sumStrike(frameList, i);
+            } else if (frameList.get(i).sumPinNumber() == fullScore) {
+                score += sumSpare(frameList, i);
+            } else {
                 score += frameList.get(i).sumPinNumber();
             }
         }
-        score += frameList.get(frameList.size() - 1).sumPinNumber();
+        score += frameList.get(theLastFrameIndex).sumPinNumber();
         return score;
     }
 
@@ -29,17 +34,12 @@ public class BowlingGame {
     }
 
     private int sumStrike(List<Frame> frameList, int index) {
-        if(index + 1 == frameList.size() - 1){
+        if (index + 1 == frameList.size() - 1 || frameList.get(index + 1).getThrowNumber() != 1) {
             return frameList.get(index).sumPinNumber() + frameList.get(index + 1).getPinNumber()[0] + frameList.get(index + 1).getPinNumber()[1];
-        }else {
-            if(frameList.get(index + 1).getThrowNumber() == 1){
-                if(index + 2 == frameList.size() - 1){
-                    return frameList.get(index).sumPinNumber() + frameList.get(index + 1).sumPinNumber() + frameList.get(index + 2).getPinNumber()[0];
-                }else {
-                    return frameList.get(index).sumPinNumber() + frameList.get(index + 1).sumPinNumber() + frameList.get(index + 2).getPinNumber()[0];
-                }
-            }
-            return frameList.get(index).sumPinNumber() + frameList.get(index + 1).getPinNumber()[0] + frameList.get(index + 1).getPinNumber()[1];
+        } else if (index + 2 == frameList.size() - 1) {
+            return frameList.get(index).sumPinNumber() + frameList.get(index + 1).sumPinNumber() + frameList.get(index + 2).getPinNumber()[0];
+        } else {
+            return frameList.get(index).sumPinNumber() + frameList.get(index + 1).sumPinNumber() + frameList.get(index + 2).getPinNumber()[0];
         }
     }
 }
